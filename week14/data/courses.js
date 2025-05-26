743baa1dc11df444a91a00a85e428a20a5ef3332
Coral-ing <2807879489@qq.com>
2025-05-26 11:01:36 +0800

    Create courses.js

---
+++ week14/data/courses.js
@@ -0,0 +1,87 @@
+{
+  "courses": [
+    {
+      "id": "ICTNWK549",
+      "title": "Design ICT security frameworks",
+      "duration": "8 weeks",
+      "price": 99.00,
+      "image": "images/courses1.jpg",
+      "assessments": [
+        "information on the security environment",
+        "laws and legislation relevant to ICT security",
+        "existing organisational security policies"
+      ],
+      "projects": [
+        "Research ICT security requirements",
+        "Conduct risk analysis",
+        "Develop ICT security policy and operational procedures"
+      ]
+    },
+    {
+      "id": "ICTPMG614",
+      "title": "Manage ICT project delivery",
+      "duration": "10 weeks",
+      "price": 69.00,
+      "image": "images/courses2.jpg",
+      "assessments": [
+         "industry software packages",
+        "an organisation's identified problem, opportunity or unfulfilled legislative need"
+      ],
+      "projects": [
+        "Manage systems analysis and design",
+        "Facilitate project-related workshops",
+        "Manage ICT project team performance"
+      ]
+    },
+    {
+      "id": "ICTNWK623",
+      "title": "Manage ICT security",
+      "duration": "6 weeks",
+      "price": 59.00,
+      "image": "images/courses3.jpg",
+      "assessments": [
+        "ICT business specifications",
+        "ICT security assurance specifications",
+        "management related scenarios"
+      ],
+      "projects": [
+        "Manage enterprise security parameters",
+        "Manage networks and telecommunications security",
+        "Implement and document enhancements"
+      ]
+    },
+    {
+      "id": "ICTSAD608",
+      "title": "Perform ICT-focused enterprise analysis",
+      "duration": "12 weeks",
+      "price": 99.00,
+      "image": "images/courses4.jpg",
+      "assessments": [
+        "applicable organisational policies and guidelines",
+        "information applicable to organisational goals, objectives, processes, model and strategies",
+        "business analysis practices and tools"
+      ],
+      "projects": [
+        "Define business need",
+        " Assess capability gaps",
+        "Determine solution approach"
+      ]
+    },
+    {
+      "id": "CICTSAS437",
+      "title": "Optimise ICT system performance",
+      "duration": "8 weeks",
+      "price": 89.00,
+      "image": "images/courses5.jpg",
+      "assessments": [
+        "tools, equipment and materials required to optimise ICT systems",
+        "industry software packages"
+      ],
+      "projects": [
+        "Determine ICT system performance",
+        "Investigate methods to improve ICT system performance",
+        "Modify ICT system to optimise performance"
+      ]
+    }
+  ]
+} 
